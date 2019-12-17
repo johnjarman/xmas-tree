4fac867e07cd8c89479ce9fcdc2b24c6418f95ea
John Jarman <jcj27@cam.ac.uk>
2019-12-17 08:29:18 +0000

    Ready for real hardware

--- websocket.py
+++ websocket.py
@@ -6,6 +6,7 @@ import websockets
 import colorzero
 import datetime
 import time
+import itertools
 from multiprocessing import Process, Queue
 from queue import Full
 
@@ -14,16 +15,41 @@ class XmasTreeHardware(Process):
     def __init__(self, queue):
         super().__init__()
         self.queue = queue
+        self.last_msg = []
 
     def run(self):
-        print('running')
         while True:
             msg = self.queue.get()
             if isinstance(msg, str):
                 if msg == 'stop':
                     break
-            print('trig')
-            time.sleep(1)
+
+            frame = msg[0]
+            brightness = msg[1]
+
+            if self.last_msg == msg:
+                print('ignore')
+
+            else:
+                self.last_msg = msg
+                # Construct SPI command (borrowed from tree.py)
+                start_of_frame = [0]*4
+                end_of_frame = [0]*5
+
+                # Max brightness = 31
+
+                            # SSSBBBBB (start, brightness)
+                brightness = 0b11100000 | brightness
+
+                pixels = [pixel.rgb_bytes[0:3] for pixel in frame]
+
+                pixels = [[brightness, b, g, r] for r, g, b in pixels]
+                pixels = [i for p in pixels for i in p]
+                data = start_of_frame + pixels + end_of_frame
+                print(data)
+
+                # Rate-limit SPI commands
+                time.sleep(1)
 
 
 class XmasTreeServer:
@@ -32,38 +58,89 @@ class XmasTreeServer:
         self.current_mode = 'manual'
         self.colour1 = colorzero.Color('#FB0')
         self.colour2 = colorzero.Color('#60F')
-        self.brightness = 0.1
+        self.brightness = 3
         self.hw_lock = False
         self.hw_queue = Queue(1)
         self.hw_process = XmasTreeHardware(self.hw_queue)
         self.hw_process.start()
 
-        self.frame = []
+        self.colour1_index = 0
+        self.colour2_range = (1,25)
 
-    #def __del__(self):
-    #    self.hw_queue.put('stop')
-    #    self.hw_process.join()
-    #    print('shutdown hw thread')
+        # Array of colorzero.Color objects, one for each LED
+        self.frame = [colorzero.Color('#000')] * 25
 
     async def frame_sender(self):
         while True:
+            # Send tuple (frame, brightness) to hw_queue for update
             try:
-                self.hw_queue.put(14, False)
+                self.hw_queue.put((self.frame, self.brightness), False)
             except Full:
-                print('full')
+                pass
+
             await asyncio.sleep(0.1)
 
     async def colour_cycle(self):
         """ Cycle through hues """
         while self.current_mode == 'colourcycle':
-            self.colour2 += colorzero.Hue(deg=1)
-            await self.send_ui_update({'colour2':self.colour2.html})
+            await self.set_colour2(self.colour2 + colorzero.Hue(deg=1))
             await asyncio.sleep(0.1)
 
     async def dawn_dusk_cycle(self):
         """ Run a slow change of colour through the day """
-        pass
+        while self.current_mode == 'dawn-dusk':
+            t = datetime.datetime.now()
+            
+            # Define some colours for different hours
+            colour_dict = {
+                0 : '#000033',
+                1 : '#000033',
+                2 : '#000033',
+                3 : '#000033',
+                4 : '#330066',
+                5 : '#663399',
+                6 : '#9966cc',
+                7 : '#cc6666',
+                8 : '#cc99ff',
+                9 : '#6666ff',
+                10: '#0066ff',
+                11: '#0099ff',
+                12: '#00ffff',
+                13: '#0066ff',
+                14: '#3333ff',
+                15: '#6633cc',
+                16: '#996666',
+                17: '#999933',
+                18: '#ff9900',
+                19: '#cc3399',
+                20: '#660099',
+                21: '#660066',
+                22: '#330066',
+                23: '#000066'
+            }
 
+            colour = colorzero.Color(colour_dict[t.hour])
+
+            if t.hour == 23:
+                t_next = 0
+            else:
+                t_next = t.hour + 1
+
+            next_colour = colorzero.Color(colour_dict[t_next])
+
+            color_gradient = (c for c in colour.gradient(next_colour, steps=60))
+        
+            await self.set_colour2(next(itertools.islice(color_gradient, t.minute, None)))
+            await asyncio.sleep(30)
+    
+    def set_classic_colours(self):
+        red = colorzero.Color('#FF0000')
+        green = colorzero.Color('#00FF00')
+        blue = colorzero.Color('#0000FF')
+        yellow = colorzero.Color('#FFFF00')
+
+        self.frame = [colorzero.Color('#FFFFFF')] + [red, green, blue, yellow]*6
+    
     def set_mode(self, mode):
         if mode != self.current_mode:
             # mode-change needed
@@ -72,8 +149,32 @@ class XmasTreeServer:
             if mode == 'colourcycle':
                 asyncio.create_task(self.colour_cycle())
 
-    async def set_colour2(self, colour):
-        pass
+            elif mode == 'dawn-dusk':
+                asyncio.create_task(self.dawn_dusk_cycle())
+
+            elif mode == 'classic':
+                self.set_classic_colours()
+
+    async def set_colour2(self, colour, no_ui_update = False):
+        self.colour2 = colour
+
+        for i in range(*self.colour2_range):
+            self.frame[i] = colour
+
+        if no_ui_update:
+            return
+
+        await self.send_ui_update({'colour2':self.colour2.html})
+
+    async def set_colour1(self, colour, no_ui_update = False):
+        self.colour1 = colour
+
+        self.frame[self.colour1_index] = colour
+
+        if no_ui_update:
+            return
+
+        await self.send_ui_update({'colour1':self.colour2.html})
 
     async def consumer(self, message):
         msg = json.loads(message)
@@ -82,17 +183,17 @@ class XmasTreeServer:
             self.set_mode(msg['mode'])
 
         if 'colour1' in msg.keys():
-            self.colour1 = colorzero.Color(msg['colour1'])
+            await self.set_colour1(colorzero.Color(msg['colour1']), True)
 
         if 'colour2' in msg.keys():
-            self.colour2 = colorzero.Color(msg['colour2'])
-            asyncio.create_task(self.set_colour2(self.colour2))
+            await self.set_colour2(colorzero.Color(msg['colour2']), True)
+
             if self.current_mode != 'manual':
                 await self.send_ui_update({'mode':'manual'})
                 self.current_mode = 'manual'
 
         if 'brightness' in msg.keys():
-            self.brightness = msg['brightness']
+            self.brightness = int(msg['brightness'])
 
         if 'cmd' in msg.keys():
             if msg['cmd'] == 'request_update':
@@ -117,6 +218,8 @@ class XmasTreeServer:
         self.websocket = websocket
         consumer_task = asyncio.create_task(self.consumer_handler())
         asyncio.create_task(self.frame_sender())
+        await self.set_colour1(colorzero.Color('#FB0'))
+        await self.set_colour2(colorzero.Color('#60F'))
         await consumer_task
 
 if __name__ == '__main__':
