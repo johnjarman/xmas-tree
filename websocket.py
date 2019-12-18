80ff2d0fbea188c56eb3fc42e2f92a8cce8a69ca
John Jarman <johncjarman@gmail.com>
2019-12-18 19:43:41 +0000

    Add correct ip

--- websocket.py
+++ websocket.py
@@ -225,7 +225,7 @@ class XmasTreeServer:
 if __name__ == '__main__':
     tree_server = XmasTreeServer()
 
-    start_server = websockets.serve(tree_server.handler, 'localhost', 6789)
+    start_server = websockets.serve(tree_server.handler, '192.168.0.73', 6789)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
